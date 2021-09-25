abc74f785ad4b40ba628d96a3938d4bb4e67ccc6
Akram_Mo <akram.mo.gb@gmail.com>
2021-09-25 13:14:24 +0200

    Adding learning folder

---
+++ platformer_project/platformer/settings.py
@@ -0,0 +1,11 @@
+class Settings:
+
+	def __init__(self):
+
+		self.WIDTH, self.HEIGHT = 800, 600
+		self.H_WIDTH, self.H_HEGHT = 400, 300
+
+		self.BLACK = (0, 0, 0)
+		self.WHITE = (255, 255, 255)
+		self.LIGHT_GREEN = (146,244,255)
+
